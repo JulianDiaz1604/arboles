bef598b841854c33fd9b5646f81ab882bdf96f77
Julian Diaz <diazjulian460@gmail.com>
2022-06-04 12:56:54 -0500

    arboles

---
+++ Arboles.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+struct Nodo{
+    int dato;
+    Nodo *izquierda;
+    Nodo *derecha;
+};
+
+void menu();
+void insertar(Nodo *&arbol, int n);
+void mostrar(Nodo *arbol, int n);
+Nodo *espejo(Nodo *arbol);
+void eliminar(Nodo *&arbol);
+
+
+int main(){
+    menu();
+}
+
+void insertar(Nodo *&arbol, int n){
+
+}
+
+void mostrar(Nodo *arbol, int n){
+
+}
+
+Nodo *espejo(Nodo *arbol){
+
+}
+
+void eliminar(Nodo *&arbol){
+
+}
